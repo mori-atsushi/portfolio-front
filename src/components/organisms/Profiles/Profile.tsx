30de6312e4c76f8ca39e535be7af1713aa511bf0
Mori Atsushi <mori@cyder.tech>
2018-12-06 08:21:53 +0900

    snsを追加

--- src/components/organisms/Profiles/Profile.tsx
+++ src/components/organisms/Profiles/Profile.tsx
@@ -2,29 +2,33 @@ import * as React from 'react';
 import styledComponents from 'styled-components';
 
 import profileJpg from '../../../statics/images/profile.jpg';
+import SnsList from '../../molecules/sns/SnsList';
 
 export default () => (
-  <Container>
-    <Title>Profile</Title>
-    <Content>
-      <Name>
-        森 篤史
-        <EnglishName>Mori Atsushi</EnglishName>
-      </Name>
-      <Introduction>
-        はじめまして。<br/>
-        筑波大学の学生で、写真が好きなエンジニアです。
-      </Introduction>
-      <Career>
-        <Date>1996</Date><Event>山口県生まれ</Event>
-        <Date>2012</Date><Event>明石高専 電気情報工学科 入学</Event>
-        <Date>2015</Date><Event>知的情報環境研究室（新井イスマイル）</Event>
-        <Date>2017</Date><Event>筑波大学 情報学群 情報メディア創成学類 編入学<br/>デジタルネイチャー研究室（落合陽一）</Event>
-        <Date>2019</Date><Event>株式会社サイバーエージェント エンジニア（予定）</Event>
-      </Career>
-    </Content>
-    <Image />
-  </Container>
+  <>
+    <Container>
+      <Title>Profile</Title>
+      <Content>
+        <Name>
+          森 篤史
+          <EnglishName>Mori Atsushi</EnglishName>
+        </Name>
+        <Introduction>
+          はじめまして。<br/>
+          筑波大学の学生で、写真が好きなエンジニアです。
+        </Introduction>
+        <Career>
+          <Date>1996</Date><Event>山口県生まれ</Event>
+          <Date>2012</Date><Event>明石高専 電気情報工学科 入学</Event>
+          <Date>2015</Date><Event>知的情報環境研究室（新井イスマイル）</Event>
+          <Date>2017</Date><Event>筑波大学 情報学群 情報メディア創成学類 編入学<br/>デジタルネイチャー研究室（落合陽一）</Event>
+          <Date>2019</Date><Event>株式会社サイバーエージェント エンジニア（予定）</Event>
+        </Career>
+      </Content>
+      <Image />
+    </Container>
+    <SnsList/>
+  </>
 );
 
 const Container = styledComponents.div`
