2200252aa35edde2fb61e81630f02efa47913cfc
Mori Atsushi <mori@cyder.tech>
2018-12-06 02:25:05 +0900

    reset cssを当てた

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './components/App';
+import 'reset-css';
 import './index.css';
+
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
