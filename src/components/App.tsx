b93e33b7d2a1d9ed311d90c7c78618060b7b64d1
Mori Atsushi <mori@cyder.tech>
2018-12-06 14:24:46 +0900

    google analyticsを設定

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Route, Router } from 'react-router-dom'
 
+import history from './history';
 import ComingSoon from './pages/ComingSoon';
 import Top from './pages/Top';
 
 class App extends React.Component {
   public render() {
     return (
-      <BrowserRouter>
+      <Router history={history}>
         <>
           <Route path='/' component={Top} exact={true} />
           <Route path='/works' component={ComingSoon} />
@@ -15,7 +16,7 @@ class App extends React.Component {
           <Route path='/blog' component={ComingSoon} />
           <Route path='/contact' component={ComingSoon} />
         </>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
