1b786b7df3a5cfa6aa2a8ca867a5318b84628358
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-11-20 11:49:28 +0900

    Add BlogPage component

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,33 +1,12 @@
 import type { NextPage, GetStaticProps } from 'next'
 import { getList } from 'src/api/blogs';
-import CommonHead from 'src/components/atoms/common-head/CommonHead'
-import Header from 'src/components/molecules/headers/Header';
-import MenuHeader from 'src/components/molecules/headers/MenuHeader';
-import Pagination from 'src/components/molecules/pagination/Pagination';
-import BlogList from 'src/components/organisms/Blogs/BlogList';
+import BlogPage from 'src/components/pages/BlogPage';
 import IBlogPagingList from 'src/entities/blogPagingList';
 import styled from 'styled-components';
 
-const Blog: NextPage<IBlogPagingList> = (props) => {
-  return (
-    <>
-      <CommonHead
-        pageTitle='Blog'
-        path='blogs' />
-      <MenuHeader />
-      <Header
-          title='Blog'
-          backgroundImage='/images/blog_header.jpg' />
-      <Content>
-        <BlogList
-          list={props.list}/>
-        <Pagination
-            pageNum={ props.pageNum }
-            currentPage={ props.currentPage } />
-      </Content>
-    </>
-  );
-};
+const Blog: NextPage<IBlogPagingList> = (props) => (
+  <BlogPage {...props} />
+);
 
 export const getStaticProps: GetStaticProps<IBlogPagingList> = async () => {
   const blogList = await getList();
--- src/pages/blog/list/[id].tsx
+++ src/pages/blog/list/[id].tsx
@@ -1,38 +1,16 @@
 import type { NextPage, GetStaticProps, GetStaticPaths } from 'next'
 import { ParsedUrlQuery } from 'querystring'
 import { getList } from 'src/api/blogs';
-import CommonHead from 'src/components/atoms/common-head/CommonHead'
-import Header from 'src/components/molecules/headers/Header';
-import MenuHeader from 'src/components/molecules/headers/MenuHeader';
-import Pagination from 'src/components/molecules/pagination/Pagination';
-import BlogList from 'src/components/organisms/Blogs/BlogList';
+import BlogPage from 'src/components/pages/BlogPage';
 import IBlogPagingList from 'src/entities/blogPagingList';
-import styled from 'styled-components';
 
 type BlogListParams = {
   id: string
 } & ParsedUrlQuery
 
-const Blog: NextPage<IBlogPagingList> = (props) => {
-  return (
-    <>
-      <CommonHead
-        pageTitle='Blog'
-        path='blogs' />
-      <MenuHeader />
-      <Header
-          title='Blog'
-          backgroundImage='/images/blog_header.jpg' />
-      <Content>
-        <BlogList
-          list={props.list}/>
-        <Pagination
-            pageNum={ props.pageNum }
-            currentPage={ props.currentPage } />
-      </Content>
-    </>
-  );
-};
+const Blog: NextPage<IBlogPagingList> = (props) => (
+  <BlogPage {...props} />
+);
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const { pageNum } = await getList();
@@ -54,11 +32,5 @@ export const getStaticProps: GetStaticProps<IBlogPagingList, BlogListParams> = a
     props: blogList
   }
 }
-
-const Content = styled.section`
-  margin: 0 auto;
-  padding: 2rem 5%;
-  max-width: 1000px;
-`;
 
 export default Blog;
