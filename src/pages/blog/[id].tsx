1c8f036d429ce2093881ad04518dc31a3c547b14
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-11-20 16:53:45 +0900

    Implement to call read api

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -6,26 +6,31 @@ import Header from 'src/components/molecules/headers/Header';
 import MenuHeader from 'src/components/molecules/headers/MenuHeader';
 import BlogDetail from 'src/components/organisms/Blogs/BlogDetail';
 import IBlog from 'src/entities/blog';
+import { useRead } from 'src/hooks/useRead';
 import styled from 'styled-components';
 
 type BlogParams = {
   id: string
 } & ParsedUrlQuery
 
-const Blog: NextPage<IBlog> = (props) => (
-  <>
-    <CommonHead
-      pageTitle='Blog'
-      path='blogs' />
-    <MenuHeader />
-    <Header
-        title='Blog'
-        backgroundImage='/images/blog_header.jpg' />
-    <Content>
-      <BlogDetail { ...props } />
-    </Content>
-  </>
-);
+const Blog: NextPage<IBlog> = (props) => {
+  useRead(props)
+
+  return (
+    <>
+      <CommonHead
+        pageTitle='Blog'
+        path='blogs' />
+      <MenuHeader />
+      <Header
+          title='Blog'
+          backgroundImage='/images/blog_header.jpg' />
+      <Content>
+        <BlogDetail { ...props } />
+      </Content>
+    </>
+  )
+}
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const list = await getAllList();
