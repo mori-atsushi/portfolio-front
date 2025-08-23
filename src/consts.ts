9c71fd8d88ee8303e3cb1e4394cb10f2276a63be
Mori Atsushi <at.sushi.tb@gmail.com>
2025-08-23 21:12:22 +0900

    Update homepage with personal profile information

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mori Atsushi';
+export const SITE_DESCRIPTION = '森 篤史のポートフォリオサイト';
