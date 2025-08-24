07775e9c755edf6ee263d0131ccaff41b0363518
Mori Atsushi <at.sushi.tb@gmail.com>
2025-08-24 19:29:28 +0900

    Prepare for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://at-sushi.work/',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		remarkPlugins: [
