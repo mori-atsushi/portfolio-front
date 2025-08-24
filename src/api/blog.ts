703c553f4b4c034eec63e1d3083be9f72ffadc9c
Mori Atsushi <at.sushi.tb@gmail.com>
2025-08-24 18:50:42 +0900

    Show prev/next articles

--- src/api/blog.ts
+++ src/api/blog.ts
@@ -3,8 +3,10 @@ import { getCollection } from 'astro:content';
 export const BLOG_PAGE_SIZE = 16;
 
 export async function getSortedBlogPosts() {
-	const allPosts = (await getCollection('blog')).sort(
-		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
-	);
+	const allPosts = (await getCollection('blog')).sort((a, b) => {
+		const dateA = a.data.updatedDate || a.data.pubDate;
+		const dateB = b.data.updatedDate || b.data.pubDate;
+		return dateB.valueOf() - dateA.valueOf(); // Sort in descending order (newest first)
+	});
 	return allPosts;
 }
