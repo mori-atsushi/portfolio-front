22f996f2bbe17b6845005547ebe4ec366dd966e0
Mori Atsushi <at.sushi.tb@gmail.com>
2025-08-24 13:27:05 +0900

    Change the code style

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,9 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			theme: "github-light",
+		},
+	},
 });
